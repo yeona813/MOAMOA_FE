84942f2e5fe5cc670c48f49d1336bbd9b6856579
yuminnnnni <ka09023@ajou.ac.kr>
2024-10-18 14:55:45 +0900

    #5 [refactor] : App 컴포넌트를 화살표 함수로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/global';
 import { theme } from './styles/theme';
 
-export const App: React.FC = () => {
+export const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <>
