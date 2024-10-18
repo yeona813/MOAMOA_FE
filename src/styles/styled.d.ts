ea7af77cb02a37f17959410f2acffe49e2165c7d
yuminnnnni <ka09023@ajou.ac.kr>
2024-10-18 14:18:13 +0900

    #5 [feat] : color, font 설정
    # Conflicts:
    #	package-lock.json
    #	package.json

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,53 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      white: string;
+      grey25: string;
+      grey50: string;
+      grey100: string;
+      grey200: string;
+      grey300: string;
+      grey400: string;
+      grey500: string;
+      grey600: string;
+      grey700: string;
+      grey800: string;
+      grey900: string;
+      blue50: string;
+      blue100: string;
+      blue200: string;
+      blue300: string;
+      blue400: string;
+      blue500: string;
+      blue600: string;
+      blue700: string;
+      blue800: string;
+      blue900: string;
+      yellow50: string;
+      yellow100: string;
+      yellow200: string;
+      yellow300: string;
+      yellow400: string;
+      yellow500: string;
+      yellow600: string;
+      yellow700: string;
+      yellow800: string;
+    };
+    fontSize: {
+      h1: string;
+      h2: string;
+      h3: string;
+      h4: string;
+      h5: string;
+      h6: string;
+      h7: string;
+      h8: string;
+      h9: string;
+    };
+    fonts: {
+      regular: string;
+    };
+  }
+}
