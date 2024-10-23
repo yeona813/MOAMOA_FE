7c83f99f4e41cf31369a98c9b6c971ac63800934
yeona <aya813@naver.com>
2024-10-23 23:25:45 +0900

    #10 [refactor] : 불필요한 파일 제거

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,12 +1,3 @@
-import { ReactComponent as HomeIcon } from '../../../../public/icons/HomeIcon.svg';
-import { ReactComponent as HamburgerIcon } from '../../../../public/icons/HamburgerIcon.svg';
-import { ReactComponent as KeywordIcon } from '../../../../public/icons/KeywordIcon.svg';
-import { ReactComponent as MyIcon } from '../../../../public/icons/MyIcon.svg';
-
 export const Footer = () => {
-  return (
-    <div>
-
-    </div>
-  );
+  return <div></div>;
 };
