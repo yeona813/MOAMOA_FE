7dfefeae1a358eb45113aa765984a4d29711e252
yeona <aya813@naver.com>
2024-10-23 23:20:36 +0900

    #10 [feat] : 공통 컴포넌트 제작

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,3 +1,24 @@
+import { Button } from '../../components/common/button/Button';
+
 export const HomePage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <a href="https://api.corecord.site/ouath2/authorization/kakao">
+        <Button buttonStyle="shadow">저장하기</Button>
+      </a>
+      <Button buttonStyle="shadow" disabled={true}>
+        저장하기
+      </Button>
+      <Button buttonStyle="shadow" icon="/icons/RecordIcon.svg">
+        기록하러 가기
+      </Button>
+      <Button buttonStyle="small">채팅 다시보기</Button>
+      <Button buttonStyle="basic">완료</Button>
+      <Button buttonStyle="basic" disabled={true}>
+        완료
+      </Button>
+      <Button buttonStyle="popupRight">계속 작성하기</Button>
+      <Button buttonStyle="popupLeft">나가기</Button>
+    </div>
+  );
 };
