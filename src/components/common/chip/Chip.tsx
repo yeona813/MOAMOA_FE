ae796e22a5a909a277e82f1f61a826de6e3d8156
yeona <aya813@naver.com>
2024-10-24 23:56:26 +0900

    #10 [feat] : isBig 값에 따른 스타일링 추가

--- src/components/common/chip/Chip.tsx
+++ src/components/common/chip/Chip.tsx
@@ -3,14 +3,20 @@ import * as S from './ChipStyle';
 interface ChipProps {
   children: React.ReactNode;
   color?: boolean;
+  isBig?: boolean;
 }
 
 /**
  *
  * @param children - chip에 들어갈 글씨
  * @param color - (optional) true일 경우 노란색
+ * @param isBig - (optinal) true일 경우 big size
  * @returns
  */
-export const Chip = ({ children, color }: ChipProps) => {
-  return <S.Chip color={color}>{children}</S.Chip>;
+export const Chip = ({ children, color, isBig }: ChipProps) => {
+  return (
+    <S.Chip isBig={isBig} color={color}>
+      {children}
+    </S.Chip>
+  );
 };
