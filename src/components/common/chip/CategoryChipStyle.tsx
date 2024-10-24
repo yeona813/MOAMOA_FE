e278b5d6fbe26c75baa2cf139869f90ec9cf294d
yeona <aya813@naver.com>
2024-10-25 00:30:06 +0900

    #10 [feat] : 공통 categoryChip 구현

---
+++ src/components/common/chip/CategoryChipStyle.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const CategoryChip = styled.div<{ isSelected: boolean }>`
+  display: inline-flex;
+  padding: 0.5rem 1rem;
+  border-radius: 6.1875rem;
+  border: ${(props) => (props.isSelected ? 'none' : `1px solid ${props.theme.colors.gray50}`)};
+  background-color: ${(props) => (props.isSelected ? props.theme.colors.gray700 : '#ffffff')};
+  color: ${(props) => (props.isSelected ? props.theme.colors.white : props.theme.colors.gray500)};
+  font-size: 0.875rem;
+  cursor: pointer;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.white};
+    background-color: ${({ theme }) => theme.colors.gray700};
+    border: none;
+  }
+`;
