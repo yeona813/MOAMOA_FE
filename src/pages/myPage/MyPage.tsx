a221a547a410d1d6091ac37f88c7acb5a04454da
yeona <aya813@naver.com>
2024-10-26 22:34:54 +0900

    #19 [feat] : 마이페이지 홈 UI 구현

--- src/pages/myPage/MyPage.tsx
+++ src/pages/myPage/MyPage.tsx
@@ -1,3 +1,13 @@
+import { Header } from '../../components/features/my/Header';
+import { Profile } from '../../components/features/my/Profile';
+import { Settings } from '../../components/features/my/Settings';
+
 export const MyPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Profile />
+      <Settings />
+    </div>
+  );
 };
