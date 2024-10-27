7a50baa42b1a9bcdfaee5f1f5e4c6bd12df68543
yeona <aya813@naver.com>
2024-10-27 21:47:43 +0900

    #10 [refactor] : 공통 note 컴포넌트 구현 및 적용

---
+++ src/components/common/note/NoteStyle.tsx
@@ -0,0 +1,48 @@
+import styled, { css } from 'styled-components';
+
+export const Note = styled.section<{ $color: 'blue' | 'yellow' }>`
+  position: relative;
+  display: flex;
+  gap: 0.5625rem;
+  width: 100%;
+  height: 11.625rem;
+  padding: 0.8125rem 0.6875rem 1.25rem 0.4375rem;
+  background-color: ${(props) =>
+    props.$color === 'yellow' ? props.theme.colors.yellow100 : '#657cff'};
+  border-radius: 0.125rem;
+  box-shadow: 0px 4px 7px 0px rgba(0, 0, 0, 0.04);
+`;
+
+export const Icon = styled.div`
+  width: 0.4375rem;
+  height: 100%;
+  background-image: url('/icons/WhiteCirclesIcon.svg');
+  background-repeat: repeat-y;
+`;
+
+const blurMixin = css`
+  position: absolute;
+  border-radius: 100%;
+  filter: blur(4.6875rem);
+`;
+
+export const BigBlur = styled.div<{ $color: 'blue' | 'yellow' }>`
+  ${blurMixin};
+  background-color: ${(props) =>
+    props.$color === 'yellow' ? props.theme.colors.yellow100 : '#657cff'};
+  bottom: 0.375rem;
+  right: -0.6875rem;
+  width: 6.25rem;
+  height: 6.25rem;
+  opacity: 0.6;
+`;
+
+export const SmallBlur = styled.div<{ $color: 'blue' | 'yellow' }>`
+  ${blurMixin};
+  background-color: ${(props) =>
+    props.$color === 'yellow' ? props.theme.colors.yellow100 : '#657cff'};
+  top: 0.8125rem;
+  left: -1rem;
+  width: 3.75rem;
+  height: 3.75rem;
+`;
--- src/components/memo/Memo.tsx
+++ src/components/memo/Memo.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent } from 'react';
 import * as S from './MemoStyle';
+import { Note } from '../common/note/Note';
 
 interface MemoProps {
   memo: string;
@@ -8,15 +9,12 @@ interface MemoProps {
 
 export const Memo = ({ memo, onChange }: MemoProps) => {
   return (
-    <S.Memo>
-      <S.Icon />
+    <Note color="yellow">
       <S.TextFiled
         placeholder="경험 당시의 상황, 행동, 문제, 결과 등을 기록해주세요."
         value={memo}
         onChange={onChange}
       />
-      <S.YellowSmallBlur />
-      <S.YellowBigBlur />
-    </S.Memo>
+    </Note>
   );
 };
