2d2dd3c681b569aebd45a940385aad3ecb90da15
yeona <aya813@naver.com>
2024-10-28 22:15:57 +0900

    #24 [feat] : RecordBottomSheet 적용

--- src/pages/listPage/ListPage.tsx
+++ src/pages/listPage/ListPage.tsx
@@ -3,6 +3,7 @@ import { Header } from '../../components/list/header/Header';
 import { Footer } from '../../components/layout/footer/Footer';
 import { Content } from '../../components/list/content/Content';
 import * as S from './ListPageStyle';
+import { RecordBottomSheet } from '../../components/common/bottomSheet/RecordBottomSheet';
 
 //@TODO
 // 1. 닉네임을 백에서 직접 받아와서 Header에 처리해야한다!
@@ -11,9 +12,14 @@ import * as S from './ListPageStyle';
 const FOLDERDATA = ['전체', '밋업 프로젝트'];
 export const ListPage = () => {
   const [selectFolder, setSelectFolder] = useState('전체');
+  const [openBottom, setOpenBottom] = useState(false);
 
   const handleSelectFolder = (folderName: string) => {
     setSelectFolder(folderName);
+  };
+
+  const toggleBottomSheet = () => {
+    setOpenBottom((prev) => !prev);
   };
 
   return (
@@ -24,8 +30,9 @@ export const ListPage = () => {
         selectFolder={selectFolder}
         onClick={handleSelectFolder}
       />
-      <Content />
+      <Content onClick={toggleBottomSheet} />
       <Footer />
+      {openBottom && <RecordBottomSheet onClick={toggleBottomSheet} />}
     </S.ListPage>
   );
 };
