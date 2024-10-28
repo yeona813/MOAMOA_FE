2540e57bedfc9d1bf4f0194811e58f942e18b6b5
yeona <aya813@naver.com>
2024-10-28 22:14:35 +0900

    #24 [feat] : 공통 BottomSheet로 분리

--- src/components/common/bottomSheet/BottomSheetStyle.tsx
+++ src/components/common/bottomSheet/BottomSheetStyle.tsx
@@ -7,6 +7,7 @@ export const Background = styled.div`
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.5);
+  z-index: 100;
 `;
 
 export const BottomSheet = styled.div<{ $type: string }>`
@@ -18,6 +19,7 @@ export const BottomSheet = styled.div<{ $type: string }>`
   padding: 1.25rem;
   border-radius: 1.25rem 1.25rem 0rem 0rem;
   background-color: ${({ theme }) => theme.colors.white};
+  z-index: 100px;
 `;
 
 export const Header = styled.header`
--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,28 +1,8 @@
 import { useState } from 'react';
-import { BottomSheet } from '../../components/common/bottomSheet/BottomSheet';
-import * as S from './HomePageStyle';
-import { SheetItem } from '../../components/home/sheetItem/SheetItem';
 import { Header } from '../../components/home/header/Header';
 import { Content } from '../../components/home/content/Content';
-import { SheetItemProps } from '../../components/home/sheetItem/SheetItem';
 import { Footer } from '../../components/layout/footer/Footer';
-
-// TODO
-// 푸터... 하 ㅜㅜㅜ 푸터야 푸터 생기면 바텀시트 위치 수정해야 함
-const SHEET_ITEMS: SheetItemProps[] = [
-  {
-    title: 'AI 채팅 기록',
-    subTitle: 'AI 대화를 통해 쉽게 기록하는',
-    color: 'blue',
-    path: '/chat',
-  },
-  {
-    title: '메모 기록',
-    subTitle: '간편하고 빠르게 기록하는',
-    color: 'yellow',
-    path: '/memo',
-  },
-];
+import { RecordBottomSheet } from '../../components/common/bottomSheet/RecordBottomSheet';
 
 export const HomePage = () => {
   const [openBottom, setOpenBottom] = useState(false);
@@ -35,22 +15,8 @@ export const HomePage = () => {
     <>
       <Header onClick={toggleBottomSheet} />
       <Content />
-      {openBottom && (
-        <BottomSheet title="경험 기록하기" type="long" onClick={toggleBottomSheet}>
-          <S.SheetContent>
-            {SHEET_ITEMS.map((item, index) => (
-              <SheetItem
-                key={index}
-                title={item.title}
-                subTitle={item.subTitle}
-                color={item.color}
-                path={item.path}
-              />
-            ))}
-          </S.SheetContent>
-        </BottomSheet>
-      )}
       <Footer />
+      {openBottom && <RecordBottomSheet onClick={toggleBottomSheet} />}
     </>
   );
 };
