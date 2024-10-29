d648ce3c82ad1dad4bcd00ab677acdbfb25c497a
yeona <aya813@naver.com>
2024-10-30 03:03:11 +0900

    #27 [feat] : 폴더 삭제 UI 구현

--- src/pages/folderPage/FolderPage.tsx
+++ src/pages/folderPage/FolderPage.tsx
@@ -4,20 +4,27 @@ import { Header } from '../../components/layout/header/Header';
 import { TabBar } from '../../components/layout/tabBar/TabBar';
 import * as S from './FolderPageStyle';
 import { FolderBottomSheet } from '../../components/common/bottomSheet/FolderBottomSheet';
+import { DetailModal } from '../../components/common/modal/DetailModal';
 
 const FOLDER_DATA = ['큐시즘 밋업', '프로젝트 공모전', '아르바이트'] as string[];
 
 export const FolderPage = () => {
   const [openBottom, setOpenBottom] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
 
   const toggleBottomSheet = () => {
     setOpenBottom((prev) => !prev);
   };
 
+  const toggleModal = () => {
+    setOpenModal((prev) => !prev);
+  };
+
   const handleEdit = () => {
     setIsEditing((prev) => !prev);
   };
+
   return (
     <div>
       <Header isTabBar={true}>
@@ -28,13 +35,7 @@ export const FolderPage = () => {
           FOLDER_DATA.map((folder) => (
             <S.ListContainer key={folder}>
               {isEditing && (
-                <S.Icon
-                  src="/icons/DeleteIcon.svg"
-                  alt="delete"
-                  onClick={() => {
-                    console.log('바보');
-                  }}
-                />
+                <S.Icon src="/icons/DeleteIcon.svg" alt="delete" onClick={toggleModal} />
               )}
               <List type="folder">
                 <h6>{folder}</h6>
@@ -48,6 +49,19 @@ export const FolderPage = () => {
         )}
       </S.Content>
       {openBottom && <FolderBottomSheet onClick={toggleBottomSheet} />}
+      {openModal && (
+        <DetailModal
+          text="폴더를 삭제 하시겠어요?"
+          description="폴더 내 모든 레코드도 함께 삭제됩니다."
+          leftButtonText="돌아가기"
+          rightButtonText="삭제하기"
+          onClickBackground={toggleModal}
+          onClickLeft={toggleModal}
+          onClickRight={() => {
+            console.log('delete 구현 해야함~');
+          }}
+        />
+      )}
     </div>
   );
 };
