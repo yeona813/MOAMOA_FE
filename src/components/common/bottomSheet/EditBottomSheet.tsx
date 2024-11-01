634029248e1c9ecfc5e79eacd5ed6b2fa2726cb8
yeona <aya813@naver.com>
2024-11-01 12:42:34 +0900

    #33 [feat] : bottomSheet들 구현

---
+++ src/components/common/bottomSheet/EditBottomSheet.tsx
@@ -0,0 +1,33 @@
+import { BottomSheet } from './BottomSheet';
+import * as S from './EditBottomSheetStyle';
+
+interface EditBottomSheetProps {
+  onClick: () => void;
+  onClickDelete: () => void;
+  onClickChange: () => void;
+}
+
+export const EditBottomSheet = ({
+  onClick,
+  onClickDelete,
+  onClickChange,
+}: EditBottomSheetProps) => {
+  return (
+    <BottomSheet onClick={onClick}>
+      <S.SheetContent>
+        <S.SheetItem>
+          <S.Icon src="/icons/EditIcon.svg" />
+          수정하기
+        </S.SheetItem>
+        <S.SheetItem $isDelete={true} onClick={onClickDelete}>
+          <S.Icon src="/icons/ReportDeleteIcon.svg" />
+          삭제하기
+        </S.SheetItem>
+        <S.SheetItem onClick={onClickChange}>
+          <S.Icon src="/icons/ChangeFolderIcon.svg" />
+          폴더 변경하기
+        </S.SheetItem>
+      </S.SheetContent>
+    </BottomSheet>
+  );
+};
