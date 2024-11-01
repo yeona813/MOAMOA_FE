8cb3148458c89101e50d238c32e63182d7107e2f
yeona <aya813@naver.com>
2024-11-02 00:49:04 +0900

    #29 [refactor] : error하나의 useSate로 통일

--- src/hooks/useNicknameValidation.ts
+++ src/hooks/useNicknameValidation.ts
@@ -2,31 +2,30 @@ import { useState } from 'react';
 
 export const useNicknameValidation = () => {
   const [nickname, setNickname] = useState('');
-  const [isError, setIsError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [error, setError] = useState({ isError: false, message: '' });
 
   const onChangeNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     const nicknameReg = /[!@#$%^&*()_\-+={}:'"\\|,.<>?/~`[\]]/g;
 
+    let errorMessage = '';
     if (nicknameReg.test(value)) {
-      setErrorMessage('특수문자는 입력이 불가능해요');
-      setIsError(true);
+      errorMessage = '특수문자는 입력이 불가능해요';
     } else if (value.length < 2 || value.length > 10) {
-      setErrorMessage('10자 이내의 닉네임을 입력해주세요');
-      setIsError(true);
-    } else {
-      setErrorMessage('');
-      setIsError(false);
+      errorMessage = '10자 이내의 닉네임을 입력해주세요';
     }
+    setError({
+      isError: Boolean(errorMessage),
+      message: errorMessage,
+    });
 
     setNickname(value);
   };
 
   return {
     nickname,
-    isError,
-    errorMessage,
+    isError: error.isError,
+    errorMessage: error.message,
     onChangeNickname,
   };
 };
