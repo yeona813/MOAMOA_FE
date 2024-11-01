bb11ad535297963a4ce8ef5c4a6628c200dc7e1e
yeona <aya813@naver.com>
2024-11-01 14:29:36 +0900

    #33 [refactor] : 반복되는 코드 중복 수정

--- src/components/common/bottomSheet/EditBottomSheet.tsx
+++ src/components/common/bottomSheet/EditBottomSheet.tsx
@@ -8,27 +8,42 @@ interface EditBottomSheetProps {
   onClickChange: () => void;
 }
 
+const EDIT_ITEM = [
+  { icon: '/icons/EditIcon.svg', label: '수정하기', actionKey: 'onClickEdit' },
+  {
+    icon: '/icons/ReportDeleteIcon.svg',
+    label: '삭제하기',
+    actionKey: 'onClickDelete',
+    isDelete: true,
+  },
+  { icon: '/icons/ChangeFolderIcon.svg', label: '폴더 변경하기', actionKey: 'onClickChange' },
+];
+
 export const EditBottomSheet = ({
   onClick,
   onClickEdit,
   onClickDelete,
   onClickChange,
 }: EditBottomSheetProps) => {
+  const actions: Record<'onClickEdit' | 'onClickDelete' | 'onClickChange', () => void> = {
+    onClickEdit,
+    onClickDelete,
+    onClickChange,
+  };
+
   return (
     <BottomSheet onClick={onClick}>
       <S.SheetContent>
-        <S.SheetItem onClick={onClickEdit}>
-          <S.Icon src="/icons/EditIcon.svg" />
-          수정하기
-        </S.SheetItem>
-        <S.SheetItem $isDelete={true} onClick={onClickDelete}>
-          <S.Icon src="/icons/ReportDeleteIcon.svg" />
-          삭제하기
-        </S.SheetItem>
-        <S.SheetItem onClick={onClickChange}>
-          <S.Icon src="/icons/ChangeFolderIcon.svg" />
-          폴더 변경하기
-        </S.SheetItem>
+        {EDIT_ITEM.map(({ icon, label, actionKey, isDelete }) => (
+          <S.SheetItem
+            key={label}
+            $isDelete={isDelete}
+            onClick={actions[actionKey as keyof typeof actions]}
+          >
+            <S.Icon src={icon} />
+            {label}
+          </S.SheetItem>
+        ))}
       </S.SheetContent>
     </BottomSheet>
   );
