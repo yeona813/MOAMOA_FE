5b763f88b10d7d91114e5cc6a3e434bb49459a60
yeona <aya813@naver.com>
2024-11-01 16:33:16 +0900

    #29 [feat] : FolderBottomSheet  input 에러 처리 적용

--- src/components/common/bottomSheet/FolderBottomSheet.tsx
+++ src/components/common/bottomSheet/FolderBottomSheet.tsx
@@ -15,9 +15,15 @@ interface FolderBottomSheetProps {
  */
 export const FolderBottomSheet = ({ onClick }: FolderBottomSheetProps) => {
   const [folderName, setFolderName] = useState('');
+  const [isError, setIsError] = useState(false);
 
   const changeFolderName = (e: ChangeEvent<HTMLInputElement>) => {
     setFolderName(e.target.value);
+    if (e.target.value.length > 15) {
+      setIsError(true);
+    } else {
+      setIsError(false);
+    }
   };
 
   return (
@@ -25,11 +31,13 @@ export const FolderBottomSheet = ({ onClick }: FolderBottomSheetProps) => {
       <S.SheetContent>
         추가할 폴더의 이름을 적어주세요.
         <Input
+          isError={isError}
+          errorMessage="15자 이내의 폴더 이름을 입력해주세요"
           placeholder="최대 15자까지 입력할 수 있어요"
           value={folderName}
           onChange={changeFolderName}
         />
-        <Button $styleType="basic" disabled={folderName === ''}>
+        <Button $styleType="basic" disabled={folderName === '' || isError}>
           완료
         </Button>
       </S.SheetContent>
