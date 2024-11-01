16f1d94437dc5eb23eb3e474c9b942d75b6c4b94
yeona <aya813@naver.com>
2024-11-01 17:19:31 +0900

    #29 [refactor] : JSdoc 추가

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -5,6 +5,12 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   errorMessage: string;
 }
 
+/**
+ *
+ * @param isError - input의 유효성 검사 에러
+ * @param errorMessage - isError가 true일 때 나오는 에러 메시지
+ * @returns
+ */
 export const Input = ({ isError, errorMessage, ...props }: InputProps) => {
   return (
     <S.Container>
