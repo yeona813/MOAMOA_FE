ef944d94104e0dfb10260f9d0404d08d4e873e73
yeona <aya813@naver.com>
2024-11-02 17:02:50 +0900

    #41 [feat] : font 오류 해결

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,9 +1,9 @@
 import * as S from './FooterStyle';
 import { useLocation, useNavigate } from 'react-router-dom';
-import HomeIcon from '/src/icons/HomeIcon.svg?react';
-import ListIcon from '/src/icons/HamburgerIcon.svg?react';
-import KeywordIcon from '/src/icons/KeywordIcon.svg?react';
-import MyIcon from '/src/icons/MyIcon.svg?react';
+import HomeIcon from '/public/icons/HomeIcon.svg?react';
+import ListIcon from '/public/icons/HamburgerIcon.svg?react';
+import KeywordIcon from '/public/icons/KeywordIcon.svg?react';
+import MyIcon from '/public/icons/MyIcon.svg?react';
 
 const FOOTER_ITEM = [
   {
