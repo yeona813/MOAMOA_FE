ed58d613754c0dcd8ff97fa22534f1de5ece03c2
yuminnnnni <ka09023@ajou.ac.kr>
2024-11-09 05:45:00 +0900

    #85 [feat] : 경험 기록 완료 페이지 UI 수정 반영

--- src/pages/recordCompletePage/RecordCompletePage.Style.ts
+++ src/pages/recordCompletePage/RecordCompletePage.Style.ts
@@ -1,28 +1,97 @@
 import styled from 'styled-components';
+import { Input as OriginalInput } from '@/components/common/input/Input';
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: 12.6875rem;
+  background-image: url('/images/HeaderImage.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+`;
 
 export const Container = styled.div`
   display: flex;
+  width: 100%;
   height: 100%;
   flex-direction: column;
-  padding: 1.25rem;
 `;
 
 export const Title = styled.h4`
-  margin-top: 1.25rem;
+  margin-top: 6rem;
+  margin-bottom: 0.75rem;
+  margin-left: 3rem;
+`;
+
+export const SubTitle = styled.p`
+  font-size: 1rem;
+  font-weight: 600;
+  line-height: 140%;
   margin-bottom: 1rem;
+  margin-left: 3rem;
+  white-space: pre-line;
 `;
 
-export const SubTitle = styled.h6`
-  margin-bottom: 1.00rem;
-  margin-top: 1.25rem;
+export const TextArea = styled.textarea`
+  font-family: 'Pretendard';
+  width: 100%;
+  min-height: 200px;
+  padding: 1rem;
+  border: none;
+  resize: none;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  &:focus {
+    outline: none;
+  }
 `;
 
-export const ButtonContainer = styled.div`
+export const Line = styled.div`
+  width: 95%;
+  border: 0.0625rem solid ${({ theme }) => theme.colors.gray50};
+  align-self: center;
+  margin-bottom: 0.625rem;
+`;
+
+export const Label = styled.p`
+  color: ${({ theme }) => theme.colors.gray900};
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 145%;
+  margin-left: 0.5rem;
+`;
+
+export const ButtonWrapper = styled.div`
   position: fixed;
-  bottom: 3.125rem;
+  bottom: 1.25rem;
+  left: 1.25rem;
+  right: 1.25rem;
+  width: calc(100% - 2.5rem);
+`;
+
+export const Form = styled.form`
   display: flex;
-  left: 50%;
-  transform: translateX(-50%);
-  justify-content: center;
-  align-items: center;
+  flex-direction: column;
+  gap: 0.625rem;
+  width: 100%;
+  padding: 0 1.25rem;
+  padding-bottom: 5rem;
+  position: relative;
+  margin-top: 1rem;
+`;
+
+export const Input = styled(OriginalInput)`
+  width: 95%;
+  align-self: center;
+  padding: 0.5rem;
+  margin-bottom: 0rem;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 145%;
+  border: none;
+  &:focus {
+    border: none;
+  }
 `;
--- src/pages/recordCompletePage/RecordCompletePage.tsx
+++ src/pages/recordCompletePage/RecordCompletePage.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import { Record } from '@/components/record/Record';
-import { Input } from '@/components/common/input/Input';
 import { SelectBox } from '@/components/common/selectbox/SelectBox';
 import { Button } from '@/components/common/button/Button';
 import { FolderBottomSheet } from '@/components/common/bottomSheet/FolderBottomSheet';
@@ -8,53 +6,78 @@ import * as S from './RecordCompletePage.Style';
 import { getFormattedDate } from '@/utils/dateUtils';
 
 export const RecordCompletePage = () => {
-  const [recordSummary] = useState('오늘의 경험 요약입니다. 이곳에 간단한 경험 요약이 들어갑니다.');
+  const [recordSummary] = useState('경쟁 서비스 기능, 사용자 인터페이스(UI), 요금제 등을 분석하고 글로벌 시장에서 주요 플레이어들의 특징을 파악한 점은 서비스 기획 직무에서 필수적인 시장 분석 능력을 잘 보여줍니다.');
   const [title, setTitle] = useState(getFormattedDate());
+  const [content, setContent] = useState(recordSummary);
   const [selectedCategory, setSelectedCategory] = useState('');
-  const [isClicked, setIsClicked] = useState(false); //isClicked 사용이 안되고 있어요!!
-  const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false); // 바텀 시트 표시 상태
+  const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
 
-  const handleButtonClick = () => {
-    setIsClicked(true);
-    console.log(isClicked);
+  const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
   };
 
-  // 카테고리 선택 처리
-  const handleSelectCategory = (category: string) => {
-    if (category === '폴더 추가하기') {
-      setIsBottomSheetOpen(true); // 바텀 시트 열기
+  const handleChangeContent = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value);
+  };
+
+  const handleChangeCategory = (category: string) => {
+    if (category === '새 폴더 추가하기') {
+      setIsBottomSheetOpen(true);
     } else {
       setSelectedCategory(category);
     }
   };
 
-  // 바텀 시트의 완료 버튼 클릭 처리
   const handleBottomSheetComplete = () => {
-    setIsBottomSheetOpen(false); // 바텀 시트 닫기
+    setIsBottomSheetOpen(false);
   };
+
+  const handleSaveButton = () => {
+    // 저장 로직 구현
+    console.log('저장됨:', { title, content, selectedCategory });
+  };
+
+  const isSaveDisabled = !title || !content || !selectedCategory;
 
   return (
     <S.Container>
-      <S.Title>경험 기록이 완료되었어요!</S.Title>
+      <S.HeaderContainer>
+        <S.Title>경험 기록이 완료되었어요!</S.Title>
+        <S.SubTitle>코코님의 경험을 모아서<br />한눈에 보기 쉽게 요약해드렸어요</S.SubTitle>
+      </S.HeaderContainer>
 
-      <S.SubTitle>경험 요약</S.SubTitle>
-      <Record>{recordSummary}</Record>
+      <S.Form>
+        <S.Input
+          placeholder={getFormattedDate()}
+          value={title}
+          onChange={handleChangeTitle}
+          isError={false}
+        />
+        <S.Line />
+        <S.TextArea
+          value={content}
+          onChange={handleChangeContent}
+        />
+        <S.Line />
+        <S.Label>경험의 카테고리를 선택해주세요.</S.Label>
+        <SelectBox
+          select={selectedCategory}
+          onChange={handleChangeCategory}
+          selectData={['큐시즘 서비스 기획', '마이리얼트립 인턴', '서비스디자인학과 팀 프로젝트', '회사문장', '새 폴더 추가하기']}
+        // placeholder="선택하기"
+        />
+        <S.ButtonWrapper>
+          <Button
+            type="submit"
+            onClick={handleSaveButton}
+            styleType="basic"
+            disabled={isSaveDisabled}
+          >
+            저장하기
+          </Button>
+        </S.ButtonWrapper>
+      </S.Form>
 
-      <S.SubTitle>경험의 제목을 적어주세요</S.SubTitle>
-      <Input value={title} onChange={(e) => setTitle(e.target.value)} isError={false} />
-      <S.SubTitle>경험의 카테고리를 선택해주세요</S.SubTitle>
-      <SelectBox
-        select={selectedCategory}
-        onChange={handleSelectCategory}
-        selectData={['카테고리1', '카테고리2', '카테고리3', '폴더 추가하기']}
-      />
-      <S.ButtonContainer>
-        <Button styleType="shadow" onClick={handleButtonClick} disabled={!selectedCategory}>
-          저장하기
-        </Button>
-      </S.ButtonContainer>
-
-      {/* 폴더 추가 바텀 시트 */}
       {isBottomSheetOpen && (
         <FolderBottomSheet
           onClick={() => setIsBottomSheetOpen(false)}
