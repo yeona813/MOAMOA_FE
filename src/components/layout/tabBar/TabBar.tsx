00ff13174fdc497714e137bd33976386cd15c49a
yeona <aya813@naver.com>
2024-11-09 15:12:42 +0900

    #92 [refactor] : 탭바 수정

--- src/components/layout/tabBar/TabBar.tsx
+++ src/components/layout/tabBar/TabBar.tsx
@@ -2,11 +2,11 @@ import { useNavigate } from 'react-router-dom';
 import * as S from './TabBar.Style';
 import ArrowIcon from '@icons/ArrowIcon.svg';
 import EditIcon from '@icons/EditIcon.svg';
+import KebabIcon from '@icons/KebabIcon.svg';
 
 interface TabBarProps {
   leftText?: string;
   rightText?: string;
-  icon?: string;
   centerText?: string;
   onClick?: () => void;
   onClickBackIcon?: () => void;
@@ -18,7 +18,6 @@ interface TabBarProps {
  *
  * @param leftText - (optional) 왼쪽 자리에 들어갈 글씨
  * @param rightText - (optional) 오른쪽 자리에 들어갈 글씨
- * @param icon - (optional) 아이콘
  * @param centerText - (optional) 가운데 자리에 들어갈 글씨
  * @param onClick - (optional) rightText나 icon을 클릭 시 수행하는 함수
  * @param onClickBackIcon - (optional) 이전 버튼을 클릭 시 수행하는 함수, 전달되지 않을 경우 이전 페이지로 이동하는 함수 수행
@@ -30,7 +29,6 @@ interface TabBarProps {
 export const TabBar = ({
   leftText,
   rightText,
-  icon,
   centerText,
   onClick,
   onClickBackIcon,
@@ -58,10 +56,12 @@ export const TabBar = ({
           {rightText}
         </S.Text>
       )}
-      <S.IconContainer>
-        {isEditable && <S.Icon src={EditIcon} alt="편집 아이콘" onClick={onClickEditIcon} />}
-        {icon && <S.Icon src={icon} alt="아이콘" onClick={onClick} />}
-      </S.IconContainer>
+      {isEditable && (
+        <S.IconContainer>
+          <S.Icon src={EditIcon} alt="편집 아이콘" onClick={onClickEditIcon} />
+          <S.Icon src={KebabIcon} alt="케밥 아이콘" onClick={onClick} />
+        </S.IconContainer>
+      )}
     </S.TabBar>
   );
 };
