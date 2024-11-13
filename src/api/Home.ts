c1bfe84b42aa70892b36b7136d4b773809e41167
yeona <aya813@naver.com>
2024-11-13 10:53:14 +0900

    #39 [refactor] : 홈 페이지 API 연동 refactor

--- src/api/Home.ts
+++ src/api/Home.ts
@@ -2,9 +2,7 @@ import api from './instance';
 
 export async function getRecords() {
   try {
-    const response = await api.get('/api/records', {
-      withCredentials: true,
-    });
+    const response = await api.get('/api/records');
 
     if (response.data.is_success) {
       return response.data.recordDtoList;
