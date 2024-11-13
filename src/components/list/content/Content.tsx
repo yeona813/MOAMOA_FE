22678a537d954653eedbe02455fec9f8bae72e63
yeona <aya813@naver.com>
2024-11-13 15:23:11 +0900

    #103 [feat] : 폴더 리스트 조회 API 연동 완료

--- src/components/list/content/Content.tsx
+++ src/components/list/content/Content.tsx
@@ -2,67 +2,26 @@ import { useNavigate } from 'react-router-dom';
 import * as S from './Content.Style';
 import { List } from '../../common/list/List';
 import { Empty } from '@/components/common/empty/Empty';
+import { ListProps } from '@/types/Folder';
 
-interface ListItem {
-  id: number;
-  title: string;
-  chips: string[];
-  date: string;
+interface ContentProps {
+  listData: ListProps[];
 }
 
-const LISTDATA: ListItem[] = [
-  {
-    id: 1,
-    title: '프로젝트 진행 계획서',
-    chips: ['창의력', '커뮤니케이션', '문제 해결'],
-    date: '2024.10.25',
-  },
-  {
-    id: 2,
-    title: '프로젝트 진행 계획서',
-    chips: ['창의력', '커뮤니케이션', '문제 해결'],
-    date: '2024.10.25',
-  },
-  {
-    id: 1,
-    title: '프로젝트 진행 계획서',
-    chips: ['창의력', '커뮤니케이션', '문제 해결'],
-    date: '2024.10.25',
-  },
-  {
-    id: 2,
-    title: '프로젝트 진행 계획서',
-    chips: ['창의력', '커뮤니케이션', '문제 해결'],
-    date: '2024.10.25',
-  },
-  {
-    id: 1,
-    title: '프로젝트 진행 계획서',
-    chips: ['창의력', '커뮤니케이션', '문제 해결'],
-    date: '2024.10.25',
-  },
-  {
-    id: 2,
-    title: '프로젝트 진행 계획서',
-    chips: ['창의력', '커뮤니케이션', '문제 해결'],
-    date: '2024.10.25',
-  },
-]; // 추후 백엔드에서 받아오면 다른 방식으로 변경할 것!
-
-export const Content = () => {
+export const Content = ({ listData }: ContentProps) => {
   const navigate = useNavigate();
 
   return (
     <S.Content>
-      {LISTDATA.length > 0 ? (
-        LISTDATA.map((item, index) => (
+      {listData.length > 0 ? (
+        listData.map((item) => (
           <List
-            key={index}
+            key={item.analysisId}
             title={item.title}
-            chips={item.chips}
-            date={item.date}
+            chips={item.keywordList}
+            date={item.createdAt}
             onClick={() => {
-              navigate(`/report/${item.id}`);
+              navigate(`/report/${item.analysisId}`);
             }}
           />
         ))
--- src/pages/listPage/ListPage.tsx
+++ src/pages/listPage/ListPage.tsx
@@ -5,18 +5,20 @@ import * as S from './ListPage.Style';
 import { RecordBottomSheet } from '@components/common/bottomSheet/RecordBottomSheet';
 import { SideBar } from '@/components/common/sideBar/SideBar';
 import { FloatingButton } from '@/components/common/button/FloatingButton';
-import { getFolders } from '@/api/Folder';
-import { FolderListProps } from '@/types/Folder';
+import { getFolderLists, getFolders } from '@/api/Folder';
+import { FolderListProps, ListProps } from '@/types/Folder';
 
 //@TODO
 // 1. 닉네임을 백에서 직접 받아와서 Header에 처리해야한다!
 // 2. 경험 리스트가 아예 없을 때 에러 없이 되는지 또 다시 한번 확인하기!
 
 export const ListPage = () => {
-  const [selectFolder, setSelectFolder] = useState('전체');
+  const [selectFolder, setSelectFolder] = useState('');
+  //const [lastRecordId, setLastRecordId] = useState(0);
   const [openBottom, setOpenBottom] = useState(false);
   const [openSideBar, setOpenSideBar] = useState(false);
   const [folderList, setFolderList] = useState<FolderListProps[]>([]);
+  const [listData, setListData] = useState<ListProps[]>([]);
 
   const handleSelectFolder = (folderName: string) => {
     setSelectFolder(folderName);
@@ -36,10 +38,15 @@ export const ListPage = () => {
       if (folderList) {
         setFolderList(folderList);
       }
+
+      const listData = await getFolderLists(selectFolder);
+      if (listData) {
+        setListData(listData);
+      }
     };
 
     fetchUser();
-  }, []);
+  }, [selectFolder]);
 
   return (
     <S.ListPage>
@@ -50,7 +57,7 @@ export const ListPage = () => {
         onClick={handleSelectFolder}
         onClickSideBar={toggleSideBar}
       />
-      <Content />
+      <Content listData={listData} />
       <FloatingButton onClick={toggleBottomSheet} />
       {openBottom && <RecordBottomSheet onClick={toggleBottomSheet} />}
       {openSideBar && <SideBar onClick={toggleSideBar} />}
