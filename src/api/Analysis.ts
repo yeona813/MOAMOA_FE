e7c8a2b27327fe16d72f375474a0ee03ff19df56
yeona <aya813@naver.com>
2024-11-13 21:40:39 +0900

    #106 [feat] : [5.4] : API 생성

---
+++ src/api/Analysis.ts
@@ -0,0 +1,79 @@
+import api from './instance';
+
+/**
+ * [5.2] 역량별 경험 상세 조회
+ * @param analysisId
+ * @returns
+ */
+export async function getAnalysis(analysisId: number) {
+  try {
+    const response = await api.get(`/api/analysis/${analysisId}`);
+    if (response.data.is_success) {
+      return response.data.data;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+/**
+ * [5.3] 역량 키워드별 경험 기록 리스트 조회
+ * @param keyword
+ * @param lastRecordId
+ * @returns
+ */
+export async function getRecords(keyword: string, lastRecordId?: number) {
+  try {
+    const response = await api.get('/api/analysis/keyword', {
+      params: {
+        keyword: keyword,
+        lastRecordId: lastRecordId || 0,
+      },
+    });
+    if (response.data.is_success) {
+      return response.data.data;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+interface PatchFolderProps {
+  recordId: number;
+  folder: string;
+}
+
+/**
+ *
+ * [5.4] 폴더 변경하기
+ * @param recordId - recordId
+ * @param folder - 변경할 folder명
+ * @returns
+ */
+export async function patchFolder({ recordId, folder }: PatchFolderProps) {
+  try {
+    const response = await api.patch('/api/records/folder', {
+      recordId,
+      folder,
+    });
+    if (response.data.is_success) {
+      return response.data;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+/**
+ * [5.5] 역량 키워드 리스트 조회
+ * @returns
+ */
+export async function getKeywordList() {
+  try {
+    const response = await api.get('/api/keywords');
+    if (response.data.is_success) {
+      return response.data.data.keywordList;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
