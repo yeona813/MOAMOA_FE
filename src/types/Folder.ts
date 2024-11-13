2dd79b43bbbf9829c109f336fe5770d918af5f66
yeona <aya813@naver.com>
2024-11-13 15:23:49 +0900

    #103 [feat] : 폴더별 경험 API 연동 완료

--- src/types/Folder.ts
+++ src/types/Folder.ts
@@ -2,3 +2,12 @@ export interface FolderListProps {
   folderId: number;
   title: string;
 }
+
+export interface ListProps {
+  analysisId: number;
+  recordId: number;
+  folder: string;
+  title: string;
+  keywordList: string[];
+  createdAt: string;
+}
