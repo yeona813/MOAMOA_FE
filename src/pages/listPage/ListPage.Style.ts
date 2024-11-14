5839a51e4754b2d6a937a2583e16c9d673fffd32
yeona <aya813@naver.com>
2024-11-14 14:05:26 +0900

    #103 [feat] : 무한스크롤 구현

--- src/pages/listPage/ListPage.Style.ts
+++ src/pages/listPage/ListPage.Style.ts
@@ -3,3 +3,23 @@ import styled from 'styled-components';
 export const ListPage = styled.div`
   background-color: ${({ theme }) => theme.colors.gray25};
 `;
+
+export const Content = styled.div`
+  position: absolute;
+  top: 11.125rem;
+  left: 0;
+  width: 100%;
+  height: calc(100vh - 11.125rem);
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+  padding: 1.25rem;
+  background-color: ${({ theme }) => theme.colors.gray25};
+  overflow-y: auto;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
