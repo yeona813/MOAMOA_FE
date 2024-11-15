f882362b5b3a5c9f5bd817db04a2120b65a6f270
yeona <aya813@naver.com>
2024-11-16 00:09:52 +0900

    #112 [feat] : 홈페이지, instance 수정

--- src/api/Home.ts
+++ src/api/Home.ts
@@ -6,7 +6,7 @@ import api from './instance';
  */
 export async function getRecords() {
   try {
-    const response = await api.get('/api/records');
+    const response = await api.get('/api/records/recent');
 
     if (response.data.is_success) {
       return response.data.data.recordDtoList;
