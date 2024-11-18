c0b91842bea11169264032eadf73917f3c548cff
yeona <aya813@naver.com>
2024-11-18 11:34:31 +0900

    #124 [feat] : Header PC ver UI

--- src/components/home/header/Header.Style.ts
+++ src/components/home/header/Header.Style.ts
@@ -7,12 +7,28 @@ export const Header = styled.header`
   height: 14.5rem;
   padding: 0 1.25rem 1.25rem;
   color: ${({ theme }) => theme.colors.gray900};
-  background-image: url('/images/HeaderImage.png');
-  background-size: cover;
-  background-repeat: no-repeat;
+
+  @media screen and (min-width: 1280px) {
+    height: 100%;
+    padding: 0rem;
+  }
 `;
 
 export const Title = styled.h3`
   margin-top: 3.75rem;
   line-height: 135%;
+
+  @media screen and (min-width: 1280px) {
+    margin-top: 0rem;
+    font-size: 1.625rem;
+  }
+`;
+
+export const SheetContainer = styled.div`
+  display: none;
+  @media screen and (min-width: 1280px) {
+    display: flex;
+    gap: 1.25rem;
+    margin-top: 1.25rem;
+  }
 `;
