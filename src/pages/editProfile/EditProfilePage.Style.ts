8ccf3f5750e5a523f07902eead118172de776631
yeona <aya813@naver.com>
2024-11-20 02:42:10 +0900

    #126 [feat] : 프로필 편집 페이지 UI PC 버전으로 수정:

--- src/pages/editProfile/EditProfilePage.Style.ts
+++ src/pages/editProfile/EditProfilePage.Style.ts
@@ -1,19 +1,42 @@
 import styled from 'styled-components';
 
+export const TabBarContainer = styled.div`
+  ${(props) => props.theme.breakpoints.min} {
+    display: none;
+  }
+`;
+
 export const Content = styled.div`
-  position: absolute;
-  top: 3.75rem;
-  left: 0;
+  margin-top: 3.5rem;
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
+  height: calc(100vh - 3.5rem);
   width: 100%;
-  height: calc(100vh - 3.75rem);
   padding: 1.75rem 1.25rem;
+
+  ${(props) => props.theme.breakpoints.min} {
+    justify-content: flex-start;
+    gap: 2.8125rem;
+    margin-top: 5rem;
+    padding: 0rem 9.7rem;
+    height: auto;
+  }
+`;
+
+export const Title = styled.h3`
+  line-height: 135%;
+
+  ${(props) => props.theme.breakpoints.max} {
+    display: none;
+  }
 `;
 
 export const ButtonStyle = styled.div`
-  width: calc(100% - 2.5rem);
-  position: absolute;
-  bottom: 2.5rem;
-  left: 1.25rem;
+  ${(props) => props.theme.breakpoints.min} {
+    width: 11.0625rem;
+    position: absolute;
+    top: 38rem;
+    left: 60rem;
+  }
 `;
