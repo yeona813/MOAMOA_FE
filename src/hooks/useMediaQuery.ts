18c11e28f0d16553afbd6e6697e47d4cd893e2f6
yeona <aya813@naver.com>
2024-11-20 12:27:47 +0900

    #131 [feat] : 화면의 사이즈 모바일인지, 피씨인지 확인하는 훅 구현

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+
+export const useMediaQuery = (query: string): boolean => {
+  const [matches, setMatches] = useState<boolean>(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    const updateMatches = () => setMatches(media.matches);
+    updateMatches();
+    media.addEventListener('change', updateMatches);
+    return () => media.removeEventListener('change', updateMatches);
+  }, [query]);
+
+  return matches;
+};
