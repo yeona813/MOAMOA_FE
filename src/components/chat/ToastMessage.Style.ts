9165d46519db596445fdc354112eded5ff7e7a06
yeona <aya813@naver.com>
2024-11-21 10:54:57 +0900

    #151 [refactor] : 토스트 애니메이션 수정

--- src/components/chat/ToastMessage.Style.ts
+++ src/components/chat/ToastMessage.Style.ts
@@ -1,26 +1,29 @@
 import styled, { keyframes } from 'styled-components';
 
+// 아래에서 위로 올라오는 애니메이션 (좌우 이동 방지)
 export const fadeIn = keyframes`
-  from { opacity: 0; transform: translateY(1.25rem); }
-  to { opacity: 1; transform: translateY(0); }
+  from { opacity: 0; transform: translate(-50%, 1.25rem); }
+  to { opacity: 1; transform: translate(-50%, 0); }
 `;
 
 export const fadeOut = keyframes`
-  from { opacity: 1; transform: translateY(0); }
-  to { opacity: 0; transform: translateY(1.25rem); }
+  from { opacity: 1; transform: translate(-50%, 0); }
+  to { opacity: 0; transform: translate(-50%, 1.25rem); }
 `;
 
 export const ToastContainer = styled.div`
   position: fixed;
-  bottom: 10%;
-  left: 50%;
-  transform: translate(-50%, 0);
+  bottom: 10%; /* 화면 아래에서 시작 */
+  left: 50%; /* 화면 중앙에 위치 */
+  transform: translate(-50%, 0); /* 초기 위치를 중앙에 고정 */
   background-color: ${({ theme }) => theme.colors.gray700};
   padding: 0.625rem 1.25rem;
   border-radius: 1.25rem;
   font-size: 1rem;
-  animation: ${fadeIn} 0.5s ease-in, ${fadeOut} 0.5s ease-out;
-  animation-delay: 0s, 2.5s;
+  animation:
+    ${fadeIn} 0.5s ease-in,
+    ${fadeOut} 0.5s ease-out;
+  animation-delay: 0s, 2.5s; /* 딜레이 설정 */
   z-index: 1000;
 `;
 
