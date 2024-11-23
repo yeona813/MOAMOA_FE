f666223881e4cf4d62f2bfe1a2e82393db0e30a7
yeona <aya813@naver.com>
2024-11-23 22:30:06 +0900

    #167 [feat] : 텅  그래픽 아이콘 추가

--- src/components/common/empty/Empty.tsx
+++ src/components/common/empty/Empty.tsx
@@ -1,9 +1,10 @@
 import * as S from './Empty.style';
+import EmptyImage from '/images/EmptyImage.png';
 
 export const Empty = () => {
   return (
     <S.EmptyContainer>
-      <S.Empty />
+      <S.Empty src={EmptyImage} alt="텅 아이콘" />
       <S.EmptyMessage>아직 작성한 경험이 없어요!</S.EmptyMessage>
       <S.Description>
         모아모아와 함께 <br /> 내 경험과 역량을 모아보세요!
