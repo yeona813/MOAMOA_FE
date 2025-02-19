2b68ad4231d4c17aa5dd2b9e858be78ce29d113d
yeona813 <aya813@naver.com>
2025-02-19 12:08:31 +0000

    [feat] : PWA 적용

--- src/main.tsx
+++ src/main.tsx
@@ -5,3 +5,16 @@ import { Router } from './Router';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <RouterProvider router={Router} />
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker 등록 성공:', registration);
+      })
+      .catch((error) => {
+        console.error('Service Worker 등록 실패:', error);
+      });
+  });
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,59 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 // import fs from 'fs';
 // import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePWA({
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'moamoa',
+        short_name: 'moamoa',
+        description: 'moamoa Progressive Web App',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/images/Moamoa-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+          {
+            src: '/images/Moamoa-152x152.png',
+            sizes: '152x152',
+            type: 'image/png',
+          },
+          {
+            src: '/images/Moamoa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/images/Moamoa-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: '/images/Moamoa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': '/src',
